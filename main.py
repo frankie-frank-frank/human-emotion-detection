9fe148e113d8ab90d920b5914e9c59f021d1aa4f
Raphael Torsu <torsu.pilgrim@gmail.com>
2024-05-06 23:56:21 +0000

    dataset loading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # IMPORTS:
+import tensorflow as tf
 from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
 from tensorflow.keras.metrics import Accuacy, FalsePositives, FalseNegatives, TruePositives
 from tensorflow.keras.optimizers import Adam
@@ -6,3 +7,28 @@ from tensorflow.keras.callbacks import Callback, CSVLogger, EarlyStopping, Learn
 from tensorflow.keras.regularizers import L2, L1
 from tensorboard.plugins.hparams import api as hp
 from google.colab import drive
+
+
+# DATASET LOADING
+train_directory = "/content/dataset/Emotions Dataset/Emotions Dataset/train"
+validation_directory = "/content/dataset/Emotions Dataset/Emotions Dataset/train"
+
+tf.keras.preprocessing.image_dataset_from_directory(
+    train_directory,
+    labels='inferred',
+    label_mode='int',
+    class_names=None,
+    color_mode='rgb',
+    batch_size=32,
+    image_size=(256, 256),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation='bilinear',
+    follow_links=False,
+    crop_to_aspect_ratio=False,
+    pad_to_aspect_ratio=False,
+    data_format=None,
+    verbose=True
+)

f306745465f055de06b1eeef6df5d810678bac10
Raphael Torsu <torsu.pilgrim@gmail.com>
2024-05-07 11:17:16 +0000

    resizing and rescaling

--- main.py
+++ main.py
@@ -1,34 +1,81 @@
 # IMPORTS:
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import cv2
+from sklearn.metrics import confusion_matrix, roc_curve
+import seaborn as sns
+import datetime
+import io 
+import os
+import random
+from PIL import Image
+import albumentations as A
+import tensorflow_datasets as tfds
+import tensorflow_probability as tfp
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer, InputLyer, Conv2D, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization, GlobalAveragePooling2D, Activation, RandomFlip, RandomRotation, Rescaling, Resizing
 from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
-from tensorflow.keras.metrics import Accuacy, FalsePositives, FalseNegatives, TruePositives
+from tensorflow.keras.metrics import Accuracy, FalsePositives, FalseNegatives, TruePositives, TopKCategoricalAccuracy
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import Callback, CSVLogger, EarlyStopping, LearningRate
+from tensorflow.keras.callbacks import Callback, CSVLogger, EarlyStopping, LearningRateScheduler, ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.regularizers import L2, L1
 from tensorboard.plugins.hparams import api as hp
-from google.colab import drive
-
+from google.colab import drive, files
 
 # DATASET LOADING
 train_directory = "/content/dataset/Emotions Dataset/Emotions Dataset/train"
-validation_directory = "/content/dataset/Emotions Dataset/Emotions Dataset/train"
+val_directory = "/content/dataset/Emotions Dataset/Emotions Dataset/train"
+CLASS_NAMES = ["angry", "happy", "sad"]
+CONFIGURATION = {
+    "BATCH_SIZE": 32,
+    "IM_SIZE": 256,
+}
 
-tf.keras.preprocessing.image_dataset_from_directory(
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     train_directory,
     labels='inferred',
-    label_mode='int',
-    class_names=None,
+    label_mode='categorical',
+    class_names=CLASS_NAMES,
     color_mode='rgb',
     batch_size=32,
     image_size=(256, 256),
     shuffle=True,
-    seed=None,
-    validation_split=None,
-    subset=None,
-    interpolation='bilinear',
-    follow_links=False,
-    crop_to_aspect_ratio=False,
-    pad_to_aspect_ratio=False,
-    data_format=None,
-    verbose=True
+    seed=99, # use a number instead of None so you have same shuffling
 )
+
+val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    train_directory,
+    labels='inferred',
+    label_mode='categorical',
+    class_names=CLASS_NAMES,
+    color_mode='rgb',
+    batch_size=CONFIGURATION["BATCH_SIZE"],
+    image_size=(CONFIGURATION["IM_SIZE"], CONFIGURATION["IM_SIZE"]),
+    shuffle=True,
+    seed=99, # use a number instead of None so you have same shuffling
+)
+
+"""
+for i in val_dataset.take(1):
+  print(i)
+"""
+
+plt.figure(figsize = (12, 12))
+
+for images, labels in train_dataset.take(1):
+  for i in range(16):
+    ax = plt.subplot(4, 4, i + 1)
+    plt.imshow(images[i]/255.)
+    plt.title(CLASS_NAMES[tf.argmax(labels[i], axis = 0).numpy()])
+    plt.axis("off")
+
+# DATASET PREPARATION:
+training_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+val_dataset = val_dataset.prefetch(tf.data.AUTOTUNE)
+
+resize_rescale_layers = tf.keras.Sequential([
+  Resizing(CONFIGURATION["IM_SIZE"], CONFIGURATION["IM_SIZE"]),
+  Rescaling(1./255)
+])
